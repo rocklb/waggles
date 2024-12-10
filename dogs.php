58bfd9782aaf9f0fc94d1aa91952c9eb9f2ce0ed
rocklb <74123726+rocklb@users.noreply.github.com>
2024-12-10 01:31:05 -0600

    adding files

---
+++ dogs.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dogs</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">    
+</head>
+
+<body>
+<div class="navbar">
+    <div class="navlogo"> 
+        <!--<img src="imgs/logo2.png" alt=""> -->
+        <p>Waggles & More Pet Store</p>
+    </div>
+
+    <ul class="navmenu">
+        <li><a  href="index.php">Home</a></li>
+        <li><a class="active" href="dogs.php">Dogs</a></li>
+        <li><a href="cats.php">Cats</a></li>
+        <li><a href="fish.php">Fish</a></li>
+        <li><a href="index.php">Sale</a></li>
+    </ul>
+
+    <div class="navlogincart">
+        <button>Login</button>
+        <a href=""> <i class="fa-solid fa-cart-shopping"></i></a>
+        <div class="navcartcount">0</div>
+    </div>
+</div>    
+
+<div class="breadcrumbs">
+        <a href="index.php">Home</a> &gt; <span>Dogs</span>
+    </div>
+
+<div class="main_container">
+        <div class="product">
+            <img src="imgs/dog/1411685.jpeg" alt="a">
+            <div class="product_desc">
+                <h3>product name here</h3>
+                <p>$79</p>
+                <div class="rating"></div>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+            </div>
+        </div>
+
+        <div class="product">
+            <img src="imgs/dog/1411685.jpeg" alt="a">
+            <div class="product_desc">
+                <h3>product name here</h3>
+                <p>$79</p>
+                <div class="rating"></div>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+            </div>
+        </div>
+        <div class="product">
+            <img src="imgs/dog/1811625.jpeg" alt="a">
+            <div class="product_desc">
+                <h3>product name here</h3>
+                <p>$79</p>
+                <div class="rating"></div>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+            </div>
+        </div>
+
+        <div class="product">
+            <img src="imgs/dog/5116698.jpeg" alt="a">
+            <div class="product_desc">
+                <h3>product name here</h3>
+                <p>$79</p>
+                <div class="rating"></div>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+            </div>
+        </div>
+
+        <div class="product">
+            <img src="imgs/dog/5200279.jpeg" alt="a">
+            <div class="product_desc">
+                <h3>product name here</h3>
+                <p>$79</p>
+                <div class="rating"></div>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star"></span>
+                <span class="fa-solid fa-star"></span>
+            </div>
+        </div>
+
+        <div class="product">
+            <img src="imgs/dog/5200279.jpeg" alt="a">
+            <div class="product_desc">
+                <h3>product name here</h3>
+                <p>$79</p>
+                <div class="rating"></div>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+            </div>
+        </div>
+
+        <div class="product">
+            <img src="imgs/dog/5200279.jpeg" alt="a">   
+            <div class="product_desc">
+                <h3>product name here</h3>
+                <p>$79</p>
+                <div class="rating"></div>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+            </div>
+        </div>
+
+        <div class="product">
+            <img src="imgs/dog/5200279.jpeg" alt="a">
+            <div class="product_desc">
+                <h3>product name here</h3>
+                <p>$79</p>
+                <div class="rating"></div>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star"></span>
+            </div>
+        </div> 
+        <div class="product">
+            <img src="imgs/dog/1411685.jpeg" alt="a">
+            <div class="product_desc">
+                <h3>product name here</h3>
+                <p>$79</p>
+                <div class="rating"></div>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star checked"></span>
+                <span class="fa-solid fa-star"></span>
+            </div>
+        </div> 
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Waggles & More Pet Store</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+    crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" 
+    rel="stylesheet">
+</head>
+
+<body>
+<div class="navbar1">
+    <div class="navlogo"> 
+       <!-- <img src="imgs/logo3.png" alt="" class="logo"> -->
+        <p>Waggles & More Pet Store</p>
+    </div>
+
+    <ul class="navmenu1">
+        <li><a class="active" href="index.php">Home</a></li>
+        <li><a href="dogs.php">Dogs</a></li>
+        <li><a href="cats.php">Cats</a></li>
+        <li><a href="fish.php">Fish</a></li>
+        <li><a href="index.php">Sale</a></li>
+    </ul>
+
+    <div class="navlogincart">
+        <button>Login</button>
+        <a href=""> <i class="fa-solid fa-cart-shopping"></i></a>
+        <div class="navcartcount">0</div>
+    </div>
+</div>
+
+<section id="hero">
+  <h4>Shop Our Winter Sale</h4>
+  <h3>Best Prices of the Season!</h3>
+  <p>Shop for all your family's wishes!</p>
+  <button>Shop Now</button>
+</section>
+
+<section id="featured" class="section-p1">
+  <h2>Featured Products</h2>
+  <p>Shop the Most Liked Styles</p>
+  <div class="pro-container">
+    <div class="pro">
+      <img src="imgs/5279455.jpeg" alt="">
+    <div class="desc">
+      <span>Blouse1</span>
+      <h5>change this* and this^ here</h5>
+      <div class="stars">
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+      </div>
+      <h4>$68</h4>
+    </div>
+    <a href="#"><i class="fa-solid fa-cart-shopping cart cart"></i>  </a>
+
+    </div>
+
+    <div class="pro">
+      <img src="imgs/women/tops9.jpeg" alt="">
+    <div class="desc">
+      <span>Blouse1</span>
+      <h5>change this* and this^ here</h5>
+      <div class="stars">
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+      </div>
+      <h4>$68</h4>
+    </div>
+    <a href="#"><i class="fa-solid fa-cart-shopping cart cart"></i>  </a>
+
+    </div>
+
+    <div class="pro">
+      <img src="imgs/women/pj5.jpeg" alt="">
+    <div class="desc">
+      <span>Blouse1</span>
+      <h5>change this* and this^ here</h5>
+      <div class="stars">
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+      </div>
+      <h4>$68</h4>
+    </div>
+    <a href="#"><i class="fa-solid fa-cart-shopping cart cart"></i>  </a>
+
+    </div>
+
+    <div class="pro">
+      <img src="imgs/women/skt1.jpeg" alt="">
+    <div class="desc">
+      <span>Blouse1</span>
+      <h5>change this* and this^ here</h5>
+      <div class="stars">
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+      </div>
+      <h4>$68</h4>
+    </div>
+    <a href="#"><i class="fa-solid fa-cart-shopping cart cart"></i>  </a>
+    </div>
+  </div>
+</section>
+
+<div class="footer p-3 text-center">
+    <p>All rights reserved!</p>
+</div>
+
+
+    <script src="https://kit.fontawesome.com/15a9a0cae1.js" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+crossorigin="anonymous"></script>
+</body>
+</html>
